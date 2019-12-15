535d2dd9314bcf3fbe34bdc90642da65cf367adb
h4x3rotab <h4x3rotab@gmail.com>
2019-12-15 21:04:45 +0800

    console.sh: test helper

--- scripts/console.sh
+++ scripts/console.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Require jq installed:
+#  brew install jq
+#  apt install jq
+
 function req {
   rand_number=$((RANDOM))
   data="${2}"
@@ -8,11 +12,44 @@ function req {
   fi
   curl -sgX POST -H "Content-Type: application/json" "http://127.0.0.1:8000/${1}" \
        -d "{\"input\":${data}, \"nonce\": {\"id\": ${rand_number}}}" \
-       | json_pp
+       | jq '.payload|fromjson'
   echo
 }
 
+function req_set {
+  id=$1
+  path=$2
+  file_b64=$(cat "${path}"| base64 -w 0)
+  req set "{\"path\": \"${id}\", \"data\": \"${file_b64}\"}"
+}
+
+function set_dataset {
+  req_set "/ipfs/QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG" ./scripts/dataset.csv
+}
+
+function set_query {
+  req_set "/ipfs/QmY6yj1GsermExDXoosVE3aSPxdMNYr6aKuw3nA8LoWPRS" ./scripts/query.csv
+}
+
+function get_result {
+  path="/order/0"
+  req get "{\"path\": \"${path}\"}"
+}
+
 case $1 in
+set-dataset)
+  set_dataset
+;;
+set-query)
+  set_query
+;;
+set-all)
+  set_dataset
+  set_query
+;;
+get-result)
+  get_result
+;;
 *)
   req "$@"
 ;;
