462f5fa4706d89836261bc3c7105a1136c438e6a
Lawliet-Chan <Lawliet-Chan@github.com>
2020-03-02 10:01:01 +0800

    balance contract

---
+++ enclave/src/contracts/balance.rs
@@ -0,0 +1,68 @@
+use crate::std::collections::{BtreeMap};
+use serde::{de, Serialize, Deserialize, Serializer, Deserializer};
+use core::str::FromStr;
+use crate::std::prelude::v1::*;
+use crate::std::vec::Vec;
+
+use crate::contracts;
+use crate::types::TxRef;
+
+extern crate runtime as chain;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Balance{
+    amounts: BtreeMap<chain::AccountId, chain::Balance>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Command{
+    Transfer(TransferDetails),
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct TransferDetails{
+    amounts: BtreeMap<chain::AccountId,chain::Balance>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Request{
+    accounts: Vec<chain::AccountId>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Response{
+    amounts: BtreeMap<chain::AccountId, Option<chain::Balance>>,
+}
+
+impl Balance{
+    pub fn new() -> Self{
+        Balance{
+            amounts: BtreeMap::<chain::AccountId, chain::Balance>::new(),
+        }
+    }
+}
+
+impl contracts::Contract<Command, Request, Response> for Balance{
+    fn id(&self) -> contracts::ContractId { contracts::BALANCE }
+
+    fn handle_command(&mut self, origin: &String, txref: &TxRef, cmd: Command){
+        match cmd {
+            Command::Transfer(details) => {
+                for (account, balance) in details.amounts.iter() {
+                    self.amounts.insert(account,balance);
+                }
+            }
+        }
+    }
+
+    fn handle_query(&mut self, req: Request) -> Response{
+        // todo: should validate user id first.
+
+        let mut resp_map = BtreeMap::<chain::AccountId, Option<chain::Balance>>::new();
+        for i in &req.accounts {
+            resp_map.insert(i, self.amounts.get(i));
+        };
+        resp_map
+    }
+}
+
