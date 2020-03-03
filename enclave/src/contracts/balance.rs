17b711581a519dc0377d7dcb821bbd695942a334
Lawliet-Chan <Lawliet-Chan@github.com>
2020-03-03 13:11:23 +0800

    use wrapper to wrap accountID

--- enclave/src/contracts/balance.rs
+++ enclave/src/contracts/balance.rs
@@ -1,8 +1,8 @@
 use crate::std::collections::{BTreeMap};
-use serde::{de, Serialize, Deserialize, Serializer, Deserializer};
+use serde::{de::{self,Visitor}, Serialize, Deserialize, Serializer, Deserializer};
 use core::str::FromStr;
 use crate::std::prelude::v1::*;
-use crate::std::vec::Vec;
+use crate::std::{fmt,vec::Vec};
 
 use crate::contracts;
 use crate::types::TxRef;
@@ -11,7 +11,7 @@ extern crate runtime as chain;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Balance{
-    accounts: BTreeMap<chain::AccountId, chain::Balance>,
+    accounts: BTreeMap<AccountIdWrapper, chain::Balance>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -21,13 +21,13 @@ pub enum Command{
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct TransferDetails{
-    accounts: Vec<Account>,
+    accounts: Vec<(AccountIdWrapper,chain::Balance)>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Request{
-    #[serde(serialize_with = "se_to_str", deserialize_with = "de_from_str")]
-    account: chain::AccountId,
+    //#[serde(serialize_with = "se_to_str", deserialize_with = "de_from_str")]
+    account: AccountIdWrapper,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -35,18 +35,10 @@ pub struct Response{
     balance: chain::Balance,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Account{
-    #[serde(serialize_with = "se_to_str", deserialize_with = "de_from_str")]
-    account_id: chain::AccountId,
-
-    balance: chain::Balance,
-}
-
 impl Balance{
     pub fn new() -> Self{
         Balance{
-            accounts: BTreeMap::<chain::AccountId, chain::Balance>::new(),
+            accounts: BTreeMap::<AccountIdWrapper, chain::Balance>::new(),
         }
     }
 }
@@ -58,7 +50,7 @@ impl contracts::Contract<Command, Request, Response> for Balance{
         match cmd {
             Command::Transfer(details) => {
                 for account in details.accounts.iter() {
-                    self.accounts.insert(account.account_id, account.balance);
+                    self.accounts.insert(account.0, account.1);
                 }
             }
         }
@@ -68,7 +60,7 @@ impl contracts::Contract<Command, Request, Response> for Balance{
         // todo: should validate user id first.
 
 
-        let mut balance: chain::Balance = ();
+        let mut balance: chain::Balance = 0;
         if let Some(ba) = self.accounts.get(&req.account) {
             balance = *ba;
         }
@@ -79,12 +71,86 @@ impl contracts::Contract<Command, Request, Response> for Balance{
     }
 }
 
-fn se_to_str<S>(value: &chain::AccountId, serializer: S) -> Result<S::Ok, S::Error>
+#[derive(default)]
+pub struct AccountIdWrapper( chain::AccountId );
+
+impl<'a> AccountIdWrapper{
+    fn from(b: &'a [u8]) -> Self {
+        let mut a = AccountIdWrapper::default();
+        a.0.copy_from_slice(b);
+        a
+    }
+    fn into(&self) -> chain::AccountId {self.0}
+}
+
+impl Serialize for AccountIdWrapper{
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where S: Serializer,
+    {
+        serializer.serialize_bytes(&sp_core::crypto::AccountId32::from(self.0))
+    }
+}
+
+impl Deserialize for AccountIdWrapper{
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where D: Deserializer<'de>
+    {
+        deserializer.deserialize_bytes(AcidVisitor)
+    }
+}
+
+struct AcidVisitor;
+
+impl<'de> Visitor<'de> for AcidVisitor{
+    type Value = AccountIdWrapper;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("AccountID is [u8;32]")
+    }
+
+    fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E>
+        where E: Error,
+    {
+        if v.len() == 32 {
+            Ok(AccountIdWrapper::from(v))
+        }else {
+            Err(E::custom(format!("AccountId bytes length wrong: {}", v)))
+        }
+    }
+}
+/*
+pub struct BalanceWrapper( chain::Balance );
+
+impl BalanceWrapper{
+    pub fn into(&self) -> chain::Balance {self.0}
+}
+
+impl Serialize for BalanceWrapper{
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where S: Serializer,
+    {
+        let s = self.0.to_string();
+        String::serialize(&s, serializer)
+    }
+}
+
+impl Deserialize for BalanceWrapper{
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: Deserializer<'de>
+    {
+
+    }
+}
+*/
+
+/*
+fn se_to_str<S>(value: chain::AccountId, serializer: S) -> Result<S::Ok, S::Error>
     where S: Serializer {
     use std::fmt::Write;
     let mut s = String::new();
-    for a in value.0.iter() {
-        write!(s,"{:02x}", a)
+    for a in sp_core::crypto::AccountId32::from(value).iter() {
+        write!(s,"{:02x}", a);
     }
     String::serialize(&s, serializer)
 }
@@ -95,7 +161,7 @@ fn de_from_str<'de, D>(deserializer: D) -> Result<chain::Balance, D::Error>
     chain::AccountId::from_str(&s).map_err(de::Error::custom)
 }
 
-/*
+
 fn map_to_str<S>(value: BTreeMap<chain::AccountId,chain::Balance>, serializer: S) -> Result<S::Ok, S::Error>
     where S: Serializer {
     let mut map = serializer.serialize_map(Some(value.len()))?;
