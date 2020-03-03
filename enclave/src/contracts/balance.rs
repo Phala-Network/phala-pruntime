34e3ca014c87be1ddf4e4cefbd4d36079bfca49e
Lawliet-Chan <Lawliet-Chan@github.com>
2020-03-03 09:48:27 +0800

    serde with

--- enclave/src/contracts/balance.rs
+++ enclave/src/contracts/balance.rs
@@ -9,9 +9,15 @@ use crate::types::TxRef;
 
 extern crate runtime as chain;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize)]
+#[serde(remote = "chain::AccountId")]
+pub struct AccountIdDef{
+    #[serde(getter = "chain::AccountId::as_ref")]
+    id: [u8; 32]
+}
+
 pub struct Balance{
-    amounts: BTreeMap<chain::AccountId, chain::Balance>,
+    accounts: BTreeMap<chain::AccountId, chain::Balance>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -21,23 +27,33 @@ pub enum Command{
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct TransferDetails{
-    amounts: BTreeMap<chain::AccountId,chain::Balance>,
+    //#[serde(serialize_with = "map_to_str", deserialize_with = "map_from_str")]
+    accounts: Vec<Account>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Request{
-    accounts: Vec<chain::AccountId>,
+    account: chain::AccountId,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Response{
-    amounts: BTreeMap<chain::AccountId, Option<chain::Balance>>,
+    //#[serde(serialize_with = "map_to_str", deserialize_with = "map_from_str")]
+    balance: chain::Balance,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Account{
+    #[serde(with = "AccountIdDef")]
+    account_id: chain::AccountId,
+
+    balance: chain::Balance,
 }
 
 impl Balance{
     pub fn new() -> Self{
         Balance{
-            amounts: BTreeMap::<chain::AccountId, chain::Balance>::new(),
+            accounts: BTreeMap::<chain::AccountId, chain::Balance>::new(),
         }
     }
 }
@@ -48,8 +64,8 @@ impl contracts::Contract<Command, Request, Response> for Balance{
     fn handle_command(&mut self, origin: &String, txref: &TxRef, cmd: Command){
         match cmd {
             Command::Transfer(details) => {
-                for (account, balance) in details.amounts.iter() {
-                    self.amounts.insert(*account, *balance);
+                for account in details.accounts.iter() {
+                    self.accounts.insert(account.account_id, account.balance);
                 }
             }
         }
@@ -58,13 +74,61 @@ impl contracts::Contract<Command, Request, Response> for Balance{
     fn handle_query(&mut self, req: Request) -> Response{
         // todo: should validate user id first.
 
-        let mut resp_map = BTreeMap::<chain::AccountId, Option<chain::Balance>>::new();
-        for i in req.accounts {
-            resp_map.insert(i, self.amounts.get(&i).copied());
-        };
+
+        let mut balance = ();
+        if let Some(ba) = self.accounts.get(&req.account) {
+            balance = *ba;
+        }
+
         Response{
-            amounts: resp_map
+            balance,
         }
     }
+}
+/*
+fn map_to_str<S>(value: BTreeMap<chain::AccountId,chain::Balance>, serializer: S) -> Result<S::Ok, S::Error>
+    where S: Serializer {
+    let mut map = serializer.serialize_map(Some(value.len()))?;
+    for (k,v) in value{
+        let ks = acid_to_str(k);
+        let vs = v.to_string();
+        map.serialize_entry(ks, vs)?;
+    }
+    map.end()
+}
+
+fn map_from_str<'de, D>(deserializer: D) -> Result<BTreeMap<chain::AccountId,chain::Balance>, D::Error>
+    where D: Deserializer<'de> {
+    let s = String::deserialize(deserializer)?;
+    BTreeMap::<chain::AccountId,chain::Balance>::from_str(&s).map_err(de::Error::custom)
+}
+
+fn vec_to_str<S>(value: Vec<chain::AccountId>, serializer: S) -> Result<S::Ok, S::Error>
+    where S: Serializer {
+    let mut seq = serializer.serialize_seq(Some(value.len()))?;
+    for e in value {
+        let es = acid_to_str(e);
+        seq.serialize_element(es)?;
+    }
+    seq.end()
+}
+
+fn vec_from_str<'de, D>(deserializer: D) -> Result<Vec<chain::AccountId>, D::Error>
+    where D: Deserializer<'de>{
+
+}
+
+fn acid_to_str(acid: chain::AccountId) -> String{
+    use std::fmt::Write;
+    let mut s = String::new();
+    for a in acid.0.iter() {
+        write!(s,"{:02x}", a)
+    }
+    s
+}
+
+fn str_to_acid(s: String) -> Result<chain::AccountId,()>{
+    let b = s.as_bytes();
+    sp_core::crypto::AccountId32::try_from(b)
 }
 
