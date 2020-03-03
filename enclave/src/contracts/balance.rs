1b8e48cdfc542a5adec13f6e32bc4cb87f90cd11
Lawliet-Chan <Lawliet-Chan@github.com>
2020-03-03 15:45:05 +0800

    add Eq

--- enclave/src/contracts/balance.rs
+++ enclave/src/contracts/balance.rs
@@ -73,7 +73,7 @@ impl contracts::Contract<Command, Request, Response> for Balance{
     }
 }
 
-#[derive(Default, Debug, Ord)]
+#[derive(Default, Debug, Ord, Eq)]
 pub struct AccountIdWrapper( chain::AccountId );
 
 impl<'a> AccountIdWrapper{
